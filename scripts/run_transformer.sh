bb8a2ee0bfd852a9ce070df1ec8008de19cd57ff
Yufei Wang <1127800295@qq.com>
2026-05-29 10:44:22 +0800

    update run_transformer.sh

--- scripts/run_transformer.sh
+++ scripts/run_transformer.sh
@@ -1,13 +1,15 @@
 cd models
-python run.py \
-  --train-dir ../base_data/training \
-  --test-dir ../base_data/testing \
-  --real-test-dir ../base_data/testing \
-  --gcms-csv ../gcms_processed/gcms_food_vectors.csv \
-  --models transformer \
-  --contrastive on \
-  --window-sizes 100 \
-  --gradients 25 \
-  --epochs 90 \
-  --batch-size 32 \
-  --lr 0.001
+for lr in 0.0003 0.001 0.003; do
+  python run.py \
+    --train-dir ../base_data/training \
+    --test-dir ../base_data/testing \
+    --real-test-dir ../base_data/testing \
+    --gcms-csv ../gcms_processed/gcms_food_vectors.csv \
+    --models transformer \
+    --contrastive on \
+    --window-sizes 100 \
+    --gradients 25 \
+    --epochs 90 \
+    --batch-size 32 \
+    --lr "$lr"
+done
